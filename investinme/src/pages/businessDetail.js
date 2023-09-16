8df1986209c2a9be0fcc6344acb7f7df9f2825be
Noah Beightol <noahbeightol14@gmail.com>
2023-09-16 11:08:23 -0400

    initial framework for business page detail

---
+++ investinme/src/pages/businessDetail.js
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+import axios from 'axios';
+import './businessDetail.css';
+
+
+const businessDetail = () => {
+
+
+
+    return (
+    <div className="detail-page">
+      Business page
+    </div>
+  );
+};
+
+export default businessDetail;
