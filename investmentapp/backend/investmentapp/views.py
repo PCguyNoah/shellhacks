d6df60652ebea46107728c38b94215f795649edb
Noah Beightol <noahbeightol14@gmail.com>
2023-09-16 04:47:19 -0400

    Added user models in django

--- investmentapp/backend/investmentapp/views.py
+++ investmentapp/backend/investmentapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.db import models
 
 # Create your views here.
+
+class User(models.Model) :
+  firstName = models.CharField(max_length=100)
+  lastName = models.CharField(max_length=100)
+  email = models.EmailField(unique=true)
+  phone = models.CharField(max_length=15)
+  role = models.ForeignKey(Role, on_delete=models.CASCADE)
+
+class Entrepreneur(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=true)
+
+class Investor(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=true)
