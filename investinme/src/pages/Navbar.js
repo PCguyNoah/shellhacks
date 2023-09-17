842d259d96d77ea11bfcff7f6b05d720e5a43402
matheus-seabra <100260792+matheus-seabra@users.noreply.github.com>
2023-09-16 21:04:32 -0400

    Implemented Navbar for Website

---
+++ investinme/src/pages/Navbar.js
@@ -0,0 +1,17 @@
+// Navbar.js
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div class="flex-container">
+      
+       <a href="/home">Home</a>
+        <a href="/about">About</a>
+        <a href="/profile">Profile</a>
+      
+    </div>
+  );
+}
+
+export default Navbar;
